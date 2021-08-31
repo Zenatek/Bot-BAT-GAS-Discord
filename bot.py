be8ba4dd4fc2f491e56172fe9b978a3253a05d96
Zenatek <filippo.lapide@gmail.com>
2021-08-31 20:30:39 +0200

    Improve code

--- bot.py
+++ bot.py
@@ -23,42 +23,43 @@ def gas_current_value(url):
     else:
         print("Error")
 
-ch_id_list = list()
-
 bot = commands.Bot(command_prefix='$')
-
-@bot.command(ch_id_list = ch_id_list)
+@bot.command()
 @has_permissions(manage_channels=True)
 async def config(ctx, arg):
-
-    await ctx.guild.create_category("📈💲 BAT | Gas Tracker 💲 📈")
     cat = discord.utils.get(ctx.guild.categories, name="📈💲 BAT | Gas Tracker 💲 📈")
-    guild = ctx.message.guild
-    channel_name_updated = "USD | Gwei"
-    await guild.create_voice_channel(name=channel_name_updated, category=cat)
-    ch_id_list.append(discord.utils.get(ctx.guild.channels, name=channel_name_updated))
-    # print(ch_id_list)
-    helper = "Go to voice channel settings and set connect to False.\n\n" "Type: $start track"
-    await ctx.send(helper)
+    if(cat):
+        await ctx.send("Category already exists! Type $start tracker")
+    else:
+        await ctx.guild.create_category("📈💲 BAT | Gas Tracker 💲 📈")
+        cat = discord.utils.get(ctx.guild.categories, name="📈💲 BAT | Gas Tracker 💲 📈")
+        await ctx.send("Category create!")
+        guild = ctx.message.guild
+        channel_name_updated = "USD | Gwei"
+        await guild.create_voice_channel(name=channel_name_updated, category=cat)
+
+        await ctx.send("Channels ready to track. Type $start tracker")
 
 
-@bot.command(price_url=price_url, gas_url=gas_url, ch_id_list = ch_id_list)
+@bot.command(price_url=price_url, gas_url=gas_url)
 @has_permissions(manage_channels=True)
 
 async def start(ctx, arg):
+    cat = discord.utils.get(ctx.guild.categories, name="📈💲 BAT | Gas Tracker 💲 📈")
+    list_voice_ch = cat.channels
     starttime = time.time()
     while True:
         bat_price = bat_current_price(price_url)
         gas_value = gas_current_value(gas_url)
         channel_name_updated = str(bat_price) + " USD " + " | " + str(gas_value) + " Gwei"
         # await ctx.send(channel_name_updated)
-        # print(ch_id_list[0].id)
         print(channel_name_updated)
-        ch = discord.utils.get(ctx.guild.channels, id=ch_id_list[0].id)
+        ch = discord.utils.get(ctx.guild.channels, id=list_voice_ch[0].id)
         await ch.edit(name = channel_name_updated)
-        time.sleep(310.0 - ((time.time() - starttime) % 310.0))
+        time.sleep(360.0 - ((time.time() - starttime) % 360.0))
 
 
 if __name__ == '__main__':
     print("Running")
     bot.run(os.environ.get('TOKEN'))
+    
